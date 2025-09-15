b7f8ba8c27af5c17eea523c6cb7c55e478adcf9d
linmake <lin.web.05@gmail.com>
2025-09-15 06:24:55 -0700

    ready backend and deploying email

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+    alias: {
+      // Agrega estos alias para las importaciones de Amplify
+      'aws-amplify': 'aws-amplify',
+      '@aws-amplify/core': '@aws-amplify/core',
+      '@aws-amplify/auth': '@aws-amplify/auth',
+      '@aws-amplify/storage': '@aws-amplify/storage'
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      '@aws-amplify/ui-react',
+      'aws-amplify'
+    ],
+    exclude: ['@aws-amplify/ui-react/node_modules']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  }
 })
